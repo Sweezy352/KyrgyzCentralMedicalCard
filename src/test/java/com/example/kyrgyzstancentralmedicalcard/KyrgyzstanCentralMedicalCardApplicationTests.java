0b6e9de24224f87cf27a1363fee43c693014182e
Dautov Muslim <dautov.292009@gmail.com>
2025-12-10 09:55:24 +0600

    First commit

---
+++ src/test/java/com/example/kyrgyzstancentralmedicalcard/KyrgyzstanCentralMedicalCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kyrgyzstancentralmedicalcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyrgyzstanCentralMedicalCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
